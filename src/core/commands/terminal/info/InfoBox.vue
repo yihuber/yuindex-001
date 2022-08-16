8559acfbb30f696d1c9bfe6542312328d413fc02
yutou <micchuloy@protonmail.com>
2022-08-17 01:03:36 +0800

    Initial commit

---
+++ src/core/commands/terminal/info/InfoBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>关于本站：鱼头浏览器主页</div>
+
+  <div>作者：程序员鱼头</div>
+</template>
+
+<script setup lang="ts">
+interface InfoBoxProps {
+  seconds: string;
+}
+
+const props = withDefaults(defineProps<InfoBoxProps>(), {});
+</script>
+
+<style scoped></style>
